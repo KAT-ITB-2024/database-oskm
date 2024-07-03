5996ecec78ece792a58acff42870e57084e5f3fc
Nathania Calista <91357043+nathaniacalista01@users.noreply.github.com>
2024-07-03 23:55:32 +0700

    User and Profile Schema (#1)
    * feat : profile and user schema
    
    * feat : rebuild lib
    
    * feat: export types
    
    * fix: fix types
    
    ---------
    
    Co-authored-by: ImanuelSG <imanuelgirsang1@gmail.com>

--- dist/schema.cjs
+++ dist/schema.cjs
@@ -1,13 +1,17 @@
 'use strict';
 
+var cuid2 = require('@paralleldrive/cuid2');
 var drizzleOrm = require('drizzle-orm');
 var pgCore = require('drizzle-orm/pg-core');
 
-var i=pgCore.pgTableCreator(t=>`${t}`),n=i("user",{id:pgCore.varchar("id",{length:255}).notNull().primaryKey(),name:pgCore.varchar("name",{length:255}),email:pgCore.varchar("email",{length:255}).notNull(),emailVerified:pgCore.timestamp("emailVerified",{mode:"date",withTimezone:!0}).default(drizzleOrm.sql`CURRENT_TIMESTAMP`),image:pgCore.varchar("image",{length:255})}),f=drizzleOrm.relations(n,({many:t})=>({accounts:t(r)})),r=i("account",{userId:pgCore.varchar("userId",{length:255}).notNull().references(()=>n.id),type:pgCore.varchar("type",{length:255}).notNull(),provider:pgCore.varchar("provider",{length:255}).notNull(),providerAccountId:pgCore.varchar("providerAccountId",{length:255}).notNull(),refresh_token:pgCore.text("refresh_token"),access_token:pgCore.text("access_token"),expires_at:pgCore.integer("expires_at"),token_type:pgCore.varchar("token_type",{length:255}),scope:pgCore.varchar("scope",{length:255}),id_token:pgCore.text("id_token"),session_state:pgCore.varchar("session_state",{length:255})},t=>({compoundKey:pgCore.primaryKey({columns:[t.provider,t.providerAccountId]}),userIdIdx:pgCore.index("account_userId_idx").on(t.userId)})),_=drizzleOrm.relations(r,({one:t})=>({user:t(n,{fields:[r.userId],references:[n.id]})})),x=i("verificationToken",{identifier:pgCore.varchar("identifier",{length:255}).notNull(),token:pgCore.varchar("token",{length:255}).notNull(),expires:pgCore.timestamp("expires",{mode:"date",withTimezone:!0}).notNull()},t=>({compoundKey:pgCore.primaryKey({columns:[t.identifier,t.token]})}));
+var d=pgCore.pgTableCreator(e=>`${e}`),f=pgCore.pgEnum("fakultas",["FITB","FMIPA","FSRD","FTMD","FTTM","FTSL","FTI","SAPPK","SBM","SF","SITH","STEI"]),c=pgCore.pgEnum("role",["Peserta","Mentor","Mamet"]),y=pgCore.pgEnum("gender",["male","female"]),x=pgCore.pgEnum("campus",["Ganesha","Jatinangor","Cirebon"]),r=d("user",{id:pgCore.text("id").primaryKey().$defaultFn(cuid2.createId),nim:pgCore.varchar("nim",{length:100}).unique().notNull(),role:c("role").notNull(),password:pgCore.varchar("password",{length:255}).notNull(),createdAt:pgCore.timestamp("createdAt",{mode:"date",withTimezone:!0}).notNull().defaultNow(),updatedAt:pgCore.timestamp("updatedAt",{mode:"date",withTimezone:!0}).notNull().defaultNow()},e=>({idIdx:pgCore.index().on(e.id),nimIdx:pgCore.index().on(e.nim)})),a=d("profile",{id:pgCore.text("id").primaryKey().$defaultFn(cuid2.createId),name:pgCore.varchar("name",{length:255}).notNull(),userId:pgCore.text("userId").notNull().references(()=>r.id),faculty:f("faculty").notNull(),gender:y("gender").notNull(),campus:x("campus").notNull(),updatedAt:pgCore.timestamp("updatedAt",{mode:"date",withTimezone:!0}).notNull().defaultNow(),profileImage:pgCore.text("profileImage")},e=>({userIdIdx:pgCore.index().on(e.userId)})),h=drizzleOrm.relations(r,({many:e,one:m})=>({profile:m(a)})),w=drizzleOrm.relations(a,({one:e})=>({users:e(r,{fields:[a.userId],references:[r.id]})}));
 
-exports.accounts = r;
-exports.accountsRelations = _;
-exports.createTable = i;
-exports.users = n;
-exports.usersRelations = f;
-exports.verificationTokens = x;
+exports.campusEnum = x;
+exports.createTable = d;
+exports.facultyEnum = f;
+exports.genderEnum = y;
+exports.profiles = a;
+exports.profilesRelations = w;
+exports.roleEnum = c;
+exports.users = r;
+exports.usersRelations = h;
